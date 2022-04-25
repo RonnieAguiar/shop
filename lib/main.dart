71cacfae57f4fe2b6bc1b9fa61e79d825d8b931b
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-04-24 21:33:04 -0300

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/products_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -12,18 +15,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-            .copyWith(secondary: Colors.deepOrange),
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange),
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewPage(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          AppRoutes.productDetail: (ctx) => ProductDetailPage(),
+        },
       ),
-      home: ProductsOverviewPage(),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        AppRoutes.productDetail: (ctx) => ProductDetailPage(),
-      },
     );
   }
 }
---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shop/providers/counter.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    final provider = CounterProvider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Exemplo Contador'),
+        ),
+      ),
+      body: Column(
+        children: [
+          Text(provider?.state.value.toString() ?? '0'),
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: (){
+              setState(() => provider?.state.inc());
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.remove),
+            onPressed: (){
+              setState(() => provider?.state.dec());
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
