4975bb3d2465ea278d9a80766abafbd4e6ac41e5
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-05-17 08:01:05 -0300

    Update product_list.dart
    Filtro de favoritos

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,8 +5,21 @@ import 'product.dart';
 
 class ProductList with ChangeNotifier{
   final List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    return _showFavoriteOnly ?
+    _items.where((prod) => prod.isFavorite).toList()
+    : [..._items];
+  }
+
+  void showFavoriteOnly(){
+    _showFavoriteOnly = true;
+  }
+  
+  void showAll(){
+    _showFavoriteOnly = false;
+  }
   
   void addProduct(Product product){
     _items.add(product);
