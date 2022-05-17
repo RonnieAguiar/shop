db868452714143eadbd141b5b545b514f6974316
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-05-17 07:55:31 -0300

    PopupMenu para seleção de favorito

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import '../components/product_grid.dart';
 
+enum FilterOptions{
+  favorite,
+  all,
+}
+
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
@@ -9,6 +14,23 @@ class ProductsOverviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Minha Loja')),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.favorite,
+              ),
+              const PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue){
+              print(selectedValue);
+            },
+          )
+        ],
       ),
       body: const ProductGrid(),
     );
