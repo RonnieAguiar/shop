88bed5e5ac7c724364c5bfd4538ce4322a30ad5f
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-05-22 14:46:05 -0300

    Rota para página de carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/products_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         routes: {
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+          AppRoutes.cart:(ctx) => const CartPage(),
         },
       ),
     );
