d861ed7de877772557e211b629eefdcd14711e40
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-11-23 08:18:53 -0300

    Update products_page.dart

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/utils/app_routes.dart';
+
+import '../components/product_item.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({Key? key}) : super(key: key);
@@ -12,13 +15,26 @@ class ProductsPage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Gerenciar Produtos'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.productForm);
+              },
+            )
+          ],
         ),
         drawer: const AppDrawer(),
         body: Padding(
           padding: const EdgeInsets.all(8),
           child: ListView.builder(
-            itemBuilder: (ctx, i) => Text(products.items[i].name),
             itemCount: products.itemsCount,
+            itemBuilder: (ctx, i) => Column(
+              children: [
+                ProductItem(products.items[i]),
+                const Divider(),
+              ],
+            ),
           ),
         ));
   }
