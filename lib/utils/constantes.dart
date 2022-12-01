93bb0a1a52d1b6ca6749fd69bd1abb98bcdf9519
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-12-01 16:04:25 -0300

    Update constantes.dart

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,3 +1,4 @@
 class Constants{
   static const productBaseUrl = 'https://shop-cod3r-9bbc8-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl = 'https://shop-cod3r-9bbc8-default-rtdb.firebaseio.com/orders';
 }
