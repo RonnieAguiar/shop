dffe25cb3732d30b55d1db5b668c18a3abd4d13b
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-12-02 08:42:32 -0300

    Update orders_page.dart

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -8,19 +8,36 @@ import '../models/order_list.dart';
 class OrdersPage extends StatelessWidget {
   const OrdersPage({Key? key}) : super(key: key);
 
+  Future<void> _refreshOrders(BuildContext context){
+    return Provider.of<OrderList>(context, listen: false).loadOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+      body: RefreshIndicator(
+        onRefresh: (() => _refreshOrders(context)),
+        child: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: ((context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.error != null) {
+              return const Center(child: Text('Ocorreu um erro!'));
+            } else {
+              return Consumer<OrderList>(
+                builder: (context, orders, child) => ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+                ),
+              );
+            }
+          }),
+        ),
       ),
     );
   }
