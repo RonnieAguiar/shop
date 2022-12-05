3ed446357a0870da107aa2ff12b14addd868947c
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-12-05 07:32:26 -0300

    :ok_hand: Movendo o child para o final Widget.

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -19,16 +19,16 @@ class CartItemWidget extends StatelessWidget {
       direction: DismissDirection.endToStart,
       background: Container(
         color: Theme.of(context).colorScheme.error,
-        child: const Icon(
-          Icons.delete,
-          color: Colors.white,
-          size: 40,
-        ),
         alignment: Alignment.centerRight,
         padding: const EdgeInsets.only(right: 20),
         margin: const EdgeInsets.symmetric(
           horizontal: 15,
           vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
         ),
       ),
       confirmDismiss: (_) {
