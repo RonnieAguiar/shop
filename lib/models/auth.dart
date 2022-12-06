f9ecfaa4e26dfeae74f26f25bd86d338a63a5af0
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-12-06 20:01:06 -0300

    :recycle: refactor: Autenticação
    Refeito o método para reuso de url para SigUp e Login.
    :bug: fix: removido o token para arquivo privado.

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/utils/token.dart';
 
 class Auth with ChangeNotifier {
-  static const url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCR6c-Ks7dqJinCS6LWbjhAiggeajQdgTo';
-
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=${Token.token}';
     final response = await http.post(
       Uri.parse(url),
       body: jsonEncode({
@@ -17,5 +18,13 @@ class Auth with ChangeNotifier {
       }),
     );
     print(jsonDecode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }
