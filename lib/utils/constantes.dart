f89325e9639b57d7dd31ac8f7810ef6775bcc968
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-12-09 19:30:16 -0300

    :sparkles: feat: Add url base for favorites products

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,4 +1,5 @@
 class Constants{
+  static const userFavoritesUrl = 'https://shop-cod3r-9bbc8-default-rtdb.firebaseio.com/userFavorites';
   static const productBaseUrl = 'https://shop-cod3r-9bbc8-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl = 'https://shop-cod3r-9bbc8-default-rtdb.firebaseio.com/orders';
 }
