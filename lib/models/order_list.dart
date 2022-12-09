4417b4dd37cf9d2591b48cddb3a4e5c60568f8e0
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-12-09 18:14:17 -0300

    :recycle: refactor: Send token to get order list

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -9,7 +9,10 @@ import 'cart_item.dart';
 import 'order.dart';
 
 class OrderList with ChangeNotifier {
-  final List<Order> _items = [];
+  final String _token;
+  List<Order> _items;
+
+  OrderList(this._token, this._items);
 
   List<Order> get items {
     return [..._items];
@@ -20,13 +23,17 @@ class OrderList with ChangeNotifier {
   }
 
   Future<void> loadOrders() async {
-    _items.clear();
-    final response =
-        await http.get(Uri.parse('${Constants.orderBaseUrl}.json'));
+    final List<Order> items = [];
+
+    final response = await http
+        .get(Uri.parse('${Constants.orderBaseUrl}.json?auth=$_token'));
+
     if (response.body == "null") return;
+
     Map<String, dynamic> data = jsonDecode(response.body);
+
     data.forEach((orderId, orderData) {
-      _items.add(
+      items.add(
         Order(
           id: orderId,
           date: DateTime.parse(orderData['date']),
@@ -43,13 +50,15 @@ class OrderList with ChangeNotifier {
         ),
       );
     });
+
+    _items = items.reversed.toList();
     notifyListeners();
   }
 
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
-      Uri.parse('${Constants.orderBaseUrl}.json'),
+      Uri.parse('${Constants.orderBaseUrl}.json?auth=$_token'),
       body: jsonEncode(
         {
           'total': cart.totalAmount,
